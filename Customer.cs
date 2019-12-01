a36820f5a5d89e0d69033881c8542024ec860287
kim.tum <kim.99.tumanyan@gmail.com>
2019-12-01 11:01:05 +0400

    adds add() to customer

--- Customer.cs
+++ Customer.cs
@@ -8,12 +8,15 @@ namespace CompanyHierarchy
 {
     class Customer : Person
     {
-        private int Amount;
+        public int Amount { get; private set; }
 
         public Customer(string first, string last, string id, int amount) : base(first, last, id)
         {
             this.Amount = amount;
         }
+
+        public int Add(int amount) => this.Amount += amount;
+        
 
         public override string ToString() => base.ToString() + $"  Amount: {this.Amount}";
     }
